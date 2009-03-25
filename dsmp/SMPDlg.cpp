69206425c17905b176956d99e35917d801d81ca8
the100rabh <the100rabh@fc02e394-d817-11dd-9e03-45b345501c35>
2009-03-25 20:15:07 +0000

    Last checkin was with corrupted files. This time hopefully it is not

--- dsmp/SMPDlg.cpp
+++ dsmp/SMPDlg.cpp
@@ -76,9 +76,6 @@ BEGIN_EVENT_TABLE(SMPDlg,wxDialog)
 	
 	EVT_CLOSE(SMPDlg::OnClose)
 	EVT_DROP_FILES(SMPDlg::SMPDlgDropFiles)
-	EVT_MENU(ID_MNU_ADDFILES_1026 , SMPDlg::WxButton2Click)
-	EVT_MENU(ID_MNU_ADDFOLDER_1027 , SMPDlg::WxButton1Click)
-	EVT_TIMER(ID_WXTIMER1,SMPDlg::WxTimer1Timer)
 	EVT_MENU(ID_MNU_LOADPLAYLIST_1028 , SMPDlg::LoadPlaylistClick)
 	EVT_MENU(ID_MNU_SAVEPLAYLIST_1029 , SMPDlg::SavePlaylistClick)
 	EVT_MENU(ID_MNU_PLAY_PAUSE_1019 , SMPDlg::PlayButtonClick)
@@ -89,12 +86,15 @@ BEGIN_EVENT_TABLE(SMPDlg,wxDialog)
 	EVT_MENU(ID_MNU_SAVEPLAYLIST_1024 , SMPDlg::SavePlaylistClick)
 	EVT_UPDATE_UI(ID_MNU_SAVEPLAYLIST_1024 , SMPDlg::SavePlaylistUpdateUI0)
 	EVT_MENU(ID_MNU_LOADPLAYLIST_1025 , SMPDlg::LoadPlaylistClick)
+	EVT_MENU(ID_MNU_ADDFILES_1026 , SMPDlg::WxButton2Click)
+	EVT_MENU(ID_MNU_ADDFOLDER_1027 , SMPDlg::WxButton1Click)
+	EVT_TIMER(ID_WXTIMER1,SMPDlg::WxTimer1Timer)
 	
 	EVT_LIST_ITEM_ACTIVATED(ID_WXLISTCTRL1,SMPDlg::WxListCtrl1ItemActivated)
 	EVT_LIST_KEY_DOWN(ID_WXLISTCTRL1,SMPDlg::WxListCtrl1KeyDown)
 	EVT_BUTTON(ID_FINDPREVBUTTON,SMPDlg::FindPrevButtonClick)
 	EVT_BUTTON(ID_FINDNEXTBUTTON,SMPDlg::FindNextButtonClick)
-	EVT_TEXT_ENTER(ID_FINDTEXTEDIT,SMPDlg::FindTextEditEnter)
+	EVT_TEXT_ENTER(ID_FINDEDITTXT,SMPDlg::FindTextEditEnter)
 	EVT_BUTTON(ID_FINDBUTTON,SMPDlg::FindButtonClick)
 	
 	EVT_MEDIA_STOP(ID_WXMEDIACTRL1,SMPDlg::WxMediaCtrl1MediaStop)
@@ -250,11 +250,11 @@ void SMPDlg::CreateGUIControls()
 	SearchPanel->Show(false);
 	WxFlexGridSizer1->Add(SearchPanel,0,wxALIGN_LEFT | wxALL,5);
 
-	WxStaticText1 = new wxStaticText(SearchPanel, ID_WXSTATICTEXT1, wxT("Search for File : "), wxPoint(16, 8), wxDefaultSize, 0, wxT("WxStaticText1"));
+	WxStaticText1 = new wxStaticText(SearchPanel, ID_WXSTATICTEXT1, wxT("Search for File : "), wxPoint(17, 8), wxDefaultSize, 0, wxT("WxStaticText1"));
 
-	FindTextEdit = new wxTextCtrl(SearchPanel, ID_FINDTEXTEDIT, wxT(""), wxPoint(104, 5), wxSize(236, 22), wxTE_PROCESS_ENTER, wxDefaultValidator, wxT("FindTextEdit"));
+	FindTextEdit = new wxTextCtrl(SearchPanel, ID_FINDEDITTXT, wxT(""), wxPoint(105, 5), wxSize(236, 22), wxTE_PROCESS_ENTER, wxDefaultValidator, wxT("FindTextEdit"));
 
-	FindNextButton = new wxButton(SearchPanel, ID_FINDNEXTBUTTON, wxT("Next"), wxPoint(355, 7), wxSize(52, 19), 0, wxDefaultValidator, wxT("FindNextButton"));
+	FindNextButton = new wxButton(SearchPanel, ID_FINDNEXTBUTTON, wxT("Next"), wxPoint(356, 7), wxSize(52, 19), 0, wxDefaultValidator, wxT("FindNextButton"));
 
 	FindPrevButton = new wxButton(SearchPanel, ID_FINDPREVBUTTON, wxT("Prev"), wxPoint(420, 7), wxSize(52, 19), 0, wxDefaultValidator, wxT("FindPrevButton"));
 
@@ -264,6 +264,12 @@ void SMPDlg::CreateGUIControls()
 	WxListCtrl1->InsertColumn(0,wxT("Name"),wxLIST_FORMAT_LEFT,100 );
 	WxListCtrl1->InsertColumn(0,wxT("Directory"),wxLIST_FORMAT_LEFT,100 );
 	WxFlexGridSizer1->Add(WxListCtrl1,1,wxALIGN_LEFT | wxEXPAND | wxALL,5);
+
+	WxTimer1 = new wxTimer();
+	WxTimer1->SetOwner(this, ID_WXTIMER1);
+
+	AddMenu = new wxMenu(wxT(""));AddMenu->Append(ID_MNU_ADDFILES_1026, wxT("Add Files"), wxT(""), wxITEM_NORMAL);
+	AddMenu->Append(ID_MNU_ADDFOLDER_1027, wxT("Add Folder"), wxT(""), wxITEM_NORMAL);
 
 	WxPopupMenu1 = new wxMenu(wxT(""));WxPopupMenu1->Append(ID_MNU_PLAY_PAUSE_1019, wxT("Play/Pause"), wxT(""), wxITEM_NORMAL);
 	WxPopupMenu1->Append(ID_MNU_STOP_1020, wxT("Stop"), wxT(""), wxITEM_NORMAL);
@@ -275,12 +281,6 @@ void SMPDlg::CreateGUIControls()
 
 	PlaylistMenu = new wxMenu(wxT(""));PlaylistMenu->Append(ID_MNU_LOADPLAYLIST_1028, wxT("Load Playlist"), wxT(""), wxITEM_NORMAL);
 	PlaylistMenu->Append(ID_MNU_SAVEPLAYLIST_1029, wxT("Save Playlist"), wxT(""), wxITEM_NORMAL);
-
-	WxTimer1 = new wxTimer();
-	WxTimer1->SetOwner(this, ID_WXTIMER1);
-
-	AddMenu = new wxMenu(wxT(""));AddMenu->Append(ID_MNU_ADDFILES_1026, wxT("Add Files"), wxT(""), wxITEM_NORMAL);
-	AddMenu->Append(ID_MNU_ADDFOLDER_1027, wxT("Add Folder"), wxT(""), wxITEM_NORMAL);
 
 	SetTitle(wxT("DSMP"));
 	SetIcon(Self_SMPDlg_XPM);
